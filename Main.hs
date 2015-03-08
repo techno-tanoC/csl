f0d27dc2ef97a02a69a11e41aa3de2bfa57a713d
techno-tanoC <develop.tanoc@gmail.com>
2015-03-08 23:52:54 +0900

    implement parser

--- Main.hs
+++ Main.hs
@@ -1,23 +1,47 @@
 module Main where
 
 import Data.List
+import Control.Applicative ((<*), (*>), (<$>))
 import Text.Parsec
 import Text.Parsec.String
 
-data CSL = CSLValue String | CSLArray [CSL]
+data CSL = CSLValue String | CSLList [CSL]
     deriving Show
 
 csl :: Parser CSL
-csl = cArray <|> cValue
+csl = array <|> record
 
-cArray :: Parser CSL
-cArray = do
-    char '['
-    vals <- csl `sepBy` char ','
-    char ']'
-    return $ CSLArray vals
+array :: Parser CSL
+array = try (open *>  (return . CSLList $ []) <* close)
+        <|>
+        open *> record <* close
 
-cValue :: Parser CSL
-cValue = do
-    val <- many $ oneOf (['a'..'z'] ++ ['A'..'Z'])
-    return $ CSLValue val
+record :: Parser CSL
+record = CSLList <$> sepBy field comma
+
+field :: Parser CSL
+field = array <|> CSLValue <$> escaped <|> CSLValue <$> nonEscaped
+
+
+escaped :: Parser String
+escaped = do
+    dquote
+    vals <- many (textdata <|> open <|> close <|> comma <|> cr <|> lf <|> try (dquote *> dquote))
+    dquote
+    return $ vals
+
+nonEscaped :: Parser String
+nonEscaped = many textdata
+
+
+textdata :: Parser Char
+textdata = oneOf ("! " ++ ['#'..'+'] ++ ['-'..'Z'] ++ "\\" ++ ['^'..'~'])
+comma = char ','
+open :: Parser Char
+open = char '\x5b'
+close :: Parser Char
+close = char '\x5d'
+crlf = cr *> lf
+lf = char '\x0d'
+cr = char '\x0d'
+dquote = char '"'
